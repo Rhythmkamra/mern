4e06a1f0ad8cac43925daa2dab20cd273284fe94
Rhythmkamra <itsmerhythmk@gmail.com>
2023-12-29 18:25:12 +0530

    js added

--- javascript/l1.js
+++ javascript/l1.js
@@ -1,1 +1,23 @@
-console.log("rhythm")   
+// variable and constants
+// topic:- console.log
+// console.table
+// let,var,const
+
+
+
+const accountId = 14453
+//not able to change furthur 
+
+let accountEmail = "itmeriri@gmail.com"
+// 
+
+var pswd = "12345"
+let city = jaipur 
+
+console.log(accountId);
+// emmet for console = log
+console.table([accountEmail,accountId,pswd])
+
+
+// prefer not to use var due to error 
+// in functional and block scope
