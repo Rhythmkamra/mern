2c592f89a776a4ef94feb02885b9fddaa23e77d1
Rhythmkamra <itsmerhythmk@gmail.com>
2023-12-29 18:45:18 +0530

    add

---
+++ javascript/l2.js
@@ -0,0 +1,30 @@
+"use strict"
+// to show that we are using newer rules of the javascript
+//  DATA TYPES
+
+let name = "rhythm"
+let age = 20
+let login = true
+
+// null => its a value in  javascript representation of a khali value
+//  undefined => not defined abhi value define nhi ki
+// symbol => unique ness ke liye use hota hai
+
+
+
+// object 
+
+console.log(typeof "riri")
+
+
+
+
+
+
+
+
+
+
+
+// alert (3+3)
+// note the alert works well in the broswer but not in the node env although there is a speacial syntax available for alert in node 
